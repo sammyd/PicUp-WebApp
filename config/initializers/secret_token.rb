79f932bf8b80249fd22d5bb8938a7901d1a2ed1e
Sam Davies <sam.jc.davies@gmail.com>
2011-05-21 11:55:10 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PicUp::Application.config.secret_token = '610a2fe955a5d6d7cbb21aabb54066bc7f8a991d996510abbbab568f752ff3e7dbed7f93aba00fe9d3fa083fe7852ce8d8096ede453c41f800f177cb448cac40'
